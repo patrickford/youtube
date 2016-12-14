81d2bc4abf76a80aefe18c9d6ef428ab2e7d1ed1
Patrick Ford <patrickford@msn.com>
2016-12-13 19:47:26 -0800

    Basic setup

---
+++ app.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+  $("#search-form").submit(function(event) {
+    event.preventDefault();
+    var search = $("#search-term").val();
+    console.log(search)
+    $("#search-term").val("");
+    getVideos(search);
+  });
+
+  function getVideos(search) {
+    var params = {
+      part: 'snippet',
+      q: search,
+      key: 'AIzaSyAOlN6_KVX0PqPLsaA6raMgHhyA8DeX5Hw',
+      maxResults: 10
+    };
+    var url = 'https://www.googleapis.com/youtube/v3/search';
+    $.getJSON(url, params, function(data) { 
+      showResults(data);
+    });
+  }
+
+  function showResults(data) {
+    console.log(data);
+    $("#results").empty();
+    var html = "";
+    for (var i=0; i<data.items.length; i++) {
+      html += "<li>" + data.items[i].snippet.channelTitle + "</li>";
+    }
+    $("#results").append(html);
+  }
+
+});
